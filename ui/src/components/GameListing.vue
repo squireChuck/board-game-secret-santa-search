70c662ef03a81e64d3b3f602a1fedfe039f41ef9
squireChuck <squirechucks.codequest@gmail.com>
2018-11-02 16:40:35 -0400

    experimenting with some feature ideas

--- ui/src/components/GameListing.vue
+++ ui/src/components/GameListing.vue
@@ -1,23 +1,50 @@
 <template>
   <div>
-    <h1>Game listing</h1>
-    <input
-      type="text"
-      v-model="searchText"
-      placeholder="Search for a game..."
-    >
-    <div
-      v-for="player in playersWithFilteredGames(players)"
-      :key="player.name"
-    >
-      <h3>{{ player.name }}</h3>
-      <ul
-        v-for="game in sortedGames(filteredGames(ownedGames(player.games)))"
-        :key="game.objectname"
+    <header id="header-sm">
+      <h1>BGS3</h1>
+    </header>
+    <header id="header-lg">
+      <h1>Board Game Secret Santa Search</h1>
+    </header>
+    <section>
+      <div>
+        {{ participants }}
+      </div>
+      <label for="searchField">
+        Search for a game in the group's collection
+      </label>
+      <input
+        id="searchField"
+        type="text"
+        v-model="searchText"
       >
-        <li>{{ game.objectname }}</li>
-      </ul>
-    </div>
+      <div class="message-area">
+        See below for the full list!
+        /
+        Typing...
+        /
+        Looks like these Jim and Jane might have something like that...
+        /
+        Hooray, looks like that'd be a new addition!
+      </div>
+    </section>
+    <section>
+      <h2>
+        Game list
+      </h2>
+      <div
+        v-for="player in playersWithFilteredGames(players)"
+        :key="player.name"
+      >
+        <h3>{{ player.name }}</h3>
+        <ul
+          v-for="game in sortedGames(filteredGames(ownedGames(player.games)))"
+          :key="game.objectname"
+        >
+          <li>{{ game.objectname }}</li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -33,6 +60,32 @@ export default {
   computed: {
     lowerCaseSearch() {
       return this.searchText.toLowerCase();
+    },
+    playersWithGames() {
+      const players = this.players.filter(player => {
+        this.ownedGames(player.games) > 0;
+      });
+      return players;
+    },
+    participants() {
+      if (this.players.length < 2) {
+        return "Looks like we need some recruits for Secret Santa!";
+      }
+      const playerNames = this.players
+        .map(player => player.name)
+        .slice()
+        .sort((p1, p2) => {
+          const name1 = p1.toLowerCase();
+          const name2 = p2.toLowerCase();
+          if (name1 < name2) {
+            return -1;
+          }
+          return 1;
+        });
+      const numPlayers = playerNames.length - 1;
+      return `This year's participants include ${playerNames
+        .slice(0, numPlayers - 1)
+        .join(", ")} and ${playerNames[numPlayers]}.`;
     }
   },
   data() {
@@ -77,6 +130,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/* TODO Apply css reset somewhere. */
+@media (max-width: 767px) {
+  #header-sm {
+    display: show;
+  }
+  #header-lg {
+    display: none;
+  }
+}
+@media (min-width: 768px) {
+  #header-sm {
+    display: none;
+  }
+  #header-lg {
+    display: show;
+  }
+}
+
 h3 {
   margin: 40px 0 0;
 }
