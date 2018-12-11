c8923dae5b242e81c40188338ee58e8918b895bc
squireChuck <squirechucks.codequest@gmail.com>
2018-12-11 12:34:18 -0500

    staticResourcesPath in server/index.js, new compiled assets (no changes)

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const Koa = require('koa');
-const helmet = require('koa-helmet');
-const serve = require('koa-static');
-const path = require('path');
+const Koa = require('koa')
+const helmet = require('koa-helmet')
+const serve = require('koa-static')
+const path = require('path')
 
-const app = new Koa();
+const app = new Koa()
 
-app.use(helmet());
+app.use(helmet())
 
-app.use(serve(path.join(__dirname, '/dist')));
+const staticResourcesPath = path.join(__dirname, '/dist')
+app.use(serve(staticResourcesPath))
 
-const port = 3000;
+const port = 3000
 app.listen(port, () => {
   console.log(`Listening on localhost:${port}`)
-});
+})
